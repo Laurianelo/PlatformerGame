31e68c140d1c7b02ae14824603edc3c5b386063d
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-04-17 21:38:27 +0200

    Enemy movement (snake)

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] wayPoints;
+    public SpriteRenderer graphics;
+
+
+    private Transform targetPoint;
+    private int destPoint;
+
+    void Start()
+    {
+        targetPoint = wayPoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = targetPoint.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, targetPoint.position) < 0.3)
+        {
+            destPoint = (destPoint + 1) % wayPoints.Length;
+            targetPoint = wayPoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
