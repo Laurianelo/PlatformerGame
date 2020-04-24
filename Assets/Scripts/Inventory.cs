91c0f3398bb269c248df676b3dacac2f9cfaaf31
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-04-24 19:31:10 +0200

    Coins

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+    public Text coinsCountText;
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Many instances in Inventory");
+            return;
+        }
+        instance = this;
+    }
+
+    public void AddCoins(int _count)
+    {
+        coinsCount += _count;
+        coinsCountText.text = coinsCount.ToString();
+
+    }
+}
