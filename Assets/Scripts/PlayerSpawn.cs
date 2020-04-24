d56a2b37ce9694ddcaee822672021452c0037a66
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-04-24 21:36:28 +0200

    Add Level system

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject.FindWithTag("Player").transform.position = transform.position;
+    }
+}
