d3f5d415e813982fd890c00da3dd14553fc499ca
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-11-16 21:03:20 +0100

    Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIdPaused = false;
+    public GameObject pauseUI;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIdPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    private void Paused()
+    {
+        PlayerMovement.instance.enabled = false;
+        pauseUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIdPaused = true;
+
+    }
+
+    public void Resume()
+    {
+        PlayerMovement.instance.enabled = true;
+        pauseUI.SetActive(false);
+        Time.timeScale = 1;
+        gameIdPaused = false;
+    }
+
+    public void MainMenuBtn()
+    {
+        DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+        Resume();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
