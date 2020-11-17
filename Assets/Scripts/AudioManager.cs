6b2fccbf2c57d05a51ba00afebe92471c578c25f
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-11-17 23:19:51 +0100

    Add background music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource.clip = playlist[0];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
