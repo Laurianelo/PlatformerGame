80f39a40c28d740cbbaf309d670d000103efe236
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-11-18 21:30:32 +0100

    Add sound in game

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioClip[] playlist;
     public AudioSource audioSource;
+    private int musicIndex = 0;
     void Start()
     {
         audioSource.clip = playlist[0];
         audioSource.Play();
+        Debug.Log("apres le play");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!audioSource.isPlaying)
+        {
+            PlayNextSong();
+
+        }
+    }
+
+    private void PlayNextSong()
+    {
+        musicIndex = (musicIndex + 1) % playlist.Length;
+        audioSource.clip = playlist[musicIndex];
+        audioSource.Play();
     }
 }
