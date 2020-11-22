e30ca0bc2117a8cbf12865b34cb18cf1a5bba03b
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-11-22 10:06:48 +0100

    Chest Animation

---
+++ Assets/Chest.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class Chest : MonoBehaviour
+{
+
+    public Text pressE;
+    void Awake()
+    {
+        pressE = GameObject.FindGameObjectWithTag("PressE_txt").GetComponent<Text>();
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
